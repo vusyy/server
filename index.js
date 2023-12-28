a9a2d9dd588c9a90af620d8086239dfd7f2005fe
vusyy <vusal210bayramov@gmail.com>
2023-12-28 04:38:29 +0400

    test hosting

--- index.js
+++ index.js
@@ -27,14 +27,16 @@ const io = new Server(server, {
 io.on('connection',(socket)=> {
     console.log(`User Connected ${socket.id}`)
     socket.on('send-message', (messagee) =>{
+        const user = getUser(socket.id);
+
         console.log(messagee);
         
-        io.emit('resived-message', messagee)
+        io.to(user.room).emit('resived-message', messagee)
     })
     
     socket.on('join', ({ name, room }) => {
-        console.log(name, room)
         const { error, user } = addUser({ id: socket.id, name, room });
+        console.log(name, room)
         
         // if(error) return callback(error);
         
@@ -48,16 +50,20 @@ io.on('connection',(socket)=> {
 });
 
 //     socket.on('sendMessage', (message, callback) => {
-    //     const user = getUser(socket.id);
+//         const user = getUser(socket.id);
     
-    // io.to(user.room).emit('message', { user: user.name, text: message });
-    // io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
+//     io.to(user.room).emit('message', { user: user.name, text: message });
+//     io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
 
 //     callback();
 //   });
 
     socket.on('disconnect',()=> {
         console.log('user disconnected');
+        const user = removeUser(socket.id);
+        if (user) {
+            io.to(user.room).emit('resived-message', { user: 'Admin', message: `${user.name} has left.` });
+        }
     })
 })
 
